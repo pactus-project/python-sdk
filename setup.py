c0d0567d866b814633d65775f0d5c7629429b95c
Mostafa <mostafa.sedaghat@gmail.com>
2025-02-17 21:36:44 +0800

    chore: add zmq to requirements

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ LONG_DESCRIPTION_CONTENT_TYPE = "text/markdown"
 URL = "https://github.com/pactus-project/python-sdk"
 
 # Package dependencies
-REQUIRED = ["ripemd-hash", "grpcio", "grpcio-tools", "cryptography>=43.0"]
+REQUIRED = ["ripemd-hash", "grpcio", "grpcio-tools", "cryptography>=43.0", "zmq"]
 
 CLASSIFIERS = [
     "Development Status :: 5 - Production/Stable",
