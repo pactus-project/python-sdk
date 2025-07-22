a270754729b8c721a6abb350a2bf65a7efd34ffa
Mostafa <mostafa.sedaghat@gmail.com>
2025-07-22 17:37:29 +0800

    chore: fix linting issues

--- pactus/crypto/sss/sss.py
+++ pactus/crypto/sss/sss.py
@@ -2,10 +2,11 @@
 The following Python implementation of Shamir's secret sharing is
 released into the Public Domain under the terms of CC0 and OWFa:
 https://creativecommons.org/publicdomain/zero/1.0/
-http://www.openwebfoundation.org/legal/the-owf-1-0-agreements/owfa-1-0
+http://www.openwebfoundation.org/legal/the-owf-1-0-agreements/owfa-1-0.
 
 See the bottom few lines for usage. Tested on Python 2 and 3.
 """
+from __future__ import annotations
 
 import functools
 import random
@@ -13,9 +14,9 @@ import random
 _RINT = functools.partial(random.SystemRandom().randint, 0)
 
 
-def _eval_at(poly, x, prime):
+def _eval_at(poly: list[int], x: int, prime: int) -> int:
     """
-    Evaluates polynomial (coefficient tuple) at x, used to generate a
+    Evaluate polynomial (coefficient tuple) at x, used to generate a
     shamir pool in make_random_shares below.
     """
     accum = 0
@@ -33,7 +34,7 @@ def _extended_gcd(a: int, b: int) -> int:
     denominator modulo p and then multiplying the numerator by this
     inverse (Note: inverse of A is B such that A*B % p == 1). This can
     be computed via the extended Euclidean algorithm
-    http://en.wikipedia.org/wiki/Modular_multiplicative_inverse#Computation
+    http://en.wikipedia.org/wiki/Modular_multiplicative_inverse#Computation.
     """
     x = 0
     last_x = 1
@@ -50,7 +51,7 @@ def _extended_gcd(a: int, b: int) -> int:
 
 def _divmod(num: int, den: int, p: int) -> int:
     """
-    Compute num / den modulo prime p
+    Compute num / den modulo prime p.
 
     To explain this, the result will be such that:
     den * _divmod(num, den, p) % p == num
@@ -66,9 +67,11 @@ def _lagrange_interpolate(x: int, x_s: list[int], y_s: list[int], p: int) -> int
     k points will define a polynomial of up to kth order.
     """
     k = len(x_s)
-    assert k == len(set(x_s)), "points must be distinct"
+    if k != len(set(x_s)):
+        msg = "points must be distinct"
+        raise ValueError(msg)
 
-    def PI(vals):  # upper-case PI -- product of inputs
+    def _pi(vals: list[int]) -> int:  # upper-case PI -- product of inputs
         accum = 1
         for v in vals:
             accum *= v
@@ -79,25 +82,23 @@ def _lagrange_interpolate(x: int, x_s: list[int], y_s: list[int], p: int) -> int
     for i in range(k):
         others = list(x_s)
         cur = others.pop(i)
-        nums.append(PI(x - o for o in others))
-        dens.append(PI(cur - o for o in others))
+        nums.append(_pi(x - o for o in others))
+        dens.append(_pi(cur - o for o in others))
 
-    den = PI(dens)
+    den = _pi(dens)
     num = sum([_divmod(nums[i] * den * y_s[i] % p, dens[i], p) for i in range(k)])
 
     return (_divmod(num, den, p) + p) % p
 
 
 def make_random_shares(secret: int, minimum: int, shares: int, prime: int) -> list[tuple[int, int]]:
-    """
-    Generates a random shamir pool for a given secret, returns share points.
-    """
+    """Generate a random shamir pool for a given secret, returns share points."""
     if minimum > shares:
-        raise ValueError("Pool secret would be irrecoverable.")
+        msg = "Pool secret would be irrecoverable."
+        raise ValueError(msg)
     poly = [secret] + [_RINT(prime - 1) for i in range(minimum - 1)]
-    points = [(i, _eval_at(poly, i, prime)) for i in range(1, shares + 1)]
+    return [(i, _eval_at(poly, i, prime)) for i in range(1, shares + 1)]
 
-    return points
 
 
 def recover_secret(shares: list[tuple[int, int]], prime: int) -> int:
