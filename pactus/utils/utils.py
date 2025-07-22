4b23092ee107023cb3a4e211bbe5bee05a91bd6d
b00f <mostafa.sedaghat@gmail.com>
2025-07-22 17:38:29 +0800

    feat: Shamir's secret sharing (#49)

--- pactus/utils/utils.py
+++ pactus/utils/utils.py
@@ -17,29 +17,3 @@ def encode_from_base256_with_type(hrp: str, typ: str, data: bytes) -> str:
     converted = bech32m.convertbits(list(data), 8, 5, pad=True)
     converted = [typ, *converted]
     return bech32m.bech32_encode(hrp, converted, bech32m.Encoding.BECH32M)
-
-
-def evaluate_polynomial(c: list[int], x: int, mod: int) -> int | None:
-    """
-    Evaluate the polynomial f(x) = c[0] + c[1] * x + c[2] * x^2 + ... + c[n-1] * x^(n-1).
-
-    Args:
-        c: List of polynomial coefficients (c[0] is the constant term)
-        x: The value at which to evaluate the polynomial
-        mod: The modulus to use for the evaluation
-
-    Returns:
-        The computed value f(x) if success, None otherwise
-
-    """
-    if not c:
-        return None
-
-    if len(c) == 1:
-        return c[0]
-
-    y = c[-1]
-    for i in range(len(c) - 2, -1, -1):
-        y = (y * x + c[i]) % mod
-
-    return y
